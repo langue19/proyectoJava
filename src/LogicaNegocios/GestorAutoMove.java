0e7aefd3dec603d886f1bee020a8e4b52f9eddec
langue19 <103015269+langue19@users.noreply.github.com>
2026-09-21 04:02:36 -0300

    1

--- src/LogicaNegocios/GestorAutoMove.java
+++ src/LogicaNegocios/GestorAutoMove.java
@@ -12,7 +12,6 @@ public class GestorAutoMove {
 
     public GestorAutoMove() {
         vehiculos = new ListaS<>();
-        vehidisp = new ListaS<>();
         mPendientes = new Cola<>(); //Cola para misiones pendientes 
         mAsig_y_Final = new Cola<>(); //Cola para misiones q ya estan asignadas y finalizadas
         aux = new Cola<>();
@@ -49,7 +48,7 @@ public class GestorAutoMove {
     }
 
     public void vehiculoDisponible() {
-
+        vehidisp = new ListaS<>();
         if (!mPendientes.colaVacia()) {
             Nodo<Mision> frente = mPendientes.getFrente();
             Mision aux = frente.getDato();
